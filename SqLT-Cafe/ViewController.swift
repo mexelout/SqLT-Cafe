2c5bbc19618015f4d02b49cb960fcaba8bfc1e16
杉山元和 <mexelout@mexelout.local>
2015-04-16 17:27:54 +0900

    Initial Commit

---
+++ SqLT-Cafe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SqLT-Cafe
+//
+//  Created by 杉山元和 on 2015/04/16.
+//  Copyright (c) 2015年 __MEXEL_STUDIO__. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
